eb8499f8c83afba48be8a2634a36e7739f3671c4
hugocadene <contact@urioz.com>
2026-08-24 11:26:51 +0200

    Disable stale service worker caching

--- public/sw.js
+++ public/sw.js
@@ -1,63 +1,10 @@
-const STATIC_CACHE = 'castaneas-static-v1';
-const DYNAMIC_CACHE = 'castaneas-dynamic-v1';
-const APP_SHELL = ['/', '/manifest.webmanifest', '/favicon.ico', '/icon.svg'];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(STATIC_CACHE).then((cache) => cache.addAll(APP_SHELL))
-    );
+self.addEventListener('install', () => {
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
     event.waitUntil(
-        caches.keys().then((keys) => Promise.all(
-            keys
-                .filter((key) => ![STATIC_CACHE, DYNAMIC_CACHE].includes(key))
-                .map((key) => caches.delete(key))
-        ))
+        caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
     );
     self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-    if (event.request.method !== 'GET') {
-        return;
-    }
-
-    const requestUrl = new URL(event.request.url);
-
-    if (requestUrl.origin !== self.location.origin) {
-        return;
-    }
-
-    const isStaticAsset = requestUrl.pathname.startsWith('/build/') || /\.(?:js|css|png|jpg|jpeg|svg|ico|webp)$/i.test(requestUrl.pathname);
-
-    if (isStaticAsset) {
-        event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-
-                return fetch(event.request).then((networkResponse) => {
-                    const responseClone = networkResponse.clone();
-                    caches.open(DYNAMIC_CACHE).then((cache) => cache.put(event.request, responseClone));
-                    return networkResponse;
-                });
-            })
-        );
-
-        return;
-    }
-
-    event.respondWith(
-        fetch(event.request)
-            .then((networkResponse) => {
-                const responseClone = networkResponse.clone();
-                caches.open(DYNAMIC_CACHE).then((cache) => cache.put(event.request, responseClone));
-                return networkResponse;
-            })
-            .catch(() => caches.match(event.request).then((cachedResponse) => cachedResponse || caches.match('/')))
-    );
 });
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,8 +8,17 @@ Alpine.start();
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/sw.js').catch((error) => {
-			console.warn('Service worker registration failed.', error);
-		});
+		navigator.serviceWorker.getRegistrations()
+			.then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+			.then(() => {
+				if ('caches' in window) {
+					return caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))));
+				}
+
+				return undefined;
+			})
+			.catch((error) => {
+				console.warn('Service worker cleanup failed.', error);
+			});
 	});
 }
